4a1addf61e694b600155c6ca69b4d02aef4e8015
SIB61 <sib.sustswe@gmail.com>
2021-09-24 04:46:14 +0600

    MainScreenDone

--- app/src/main/java/com/sib/healthcare/Activities/MainActivity.java
+++ app/src/main/java/com/sib/healthcare/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.sib.healthcare.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.sib.healthcare.R;
 
@@ -12,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToConsultancy(View view) {
+    }
+
+    public void goToBloodBank(View view) {
+    }
+
+    public void goToCovid(View view) {
+    }
+
+    public void goToMedicine(View view) {
+    }
+
+    public void goToProfile(View view) {
+    }
+
+    public void goToAboutUs(View view) {
     }
 }
