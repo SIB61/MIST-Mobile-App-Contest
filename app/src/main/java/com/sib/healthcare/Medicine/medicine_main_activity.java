9c1c1e261466b1f2dfcab171da9f4dbf07f3b5d1
fahim <mdfahim1194@gmail.com>
2021-09-28 02:21:17 +0600

    covid all country info

---
+++ app/src/main/java/com/sib/healthcare/Medicine/medicine_main_activity.java
@@ -0,0 +1,16 @@
+package com.sib.healthcare.Medicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sib.healthcare.R;
+
+public class medicine_main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_main);
+    }
+}
