024bc6afb85294c596b0190522085735978a1ecb
SIB61 <sib.sustswe@gmail.com>
2021-09-30 02:53:17 +0600

    about us

---
+++ app/src/main/java/com/sib/healthcare/activities/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package com.sib.healthcare.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sib.healthcare.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
